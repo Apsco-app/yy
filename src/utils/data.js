e82d953340e3dc3ce86da688140e451b07a06b5d
Apsco-app <apscoschool@gmail.com>
2025-11-29 09:47:25 +0300

    Create data.js with regions, districts, and buses
    Added constants for regions, districts, and buses.

---
+++ src/utils/data.js
@@ -0,0 +1,19 @@
+export const REGIONS = [
+  { id: "central", name: "Central" },
+  { id: "western", name: "Western" },
+  { id: "eastern", name: "Eastern" },
+  { id: "northern", name: "Northern" },
+];
+
+export const DISTRICTS = {
+  central: [{ id: "kampala", name: "Kampala" }, { id: "wakiso", name: "Wakiso" }],
+  western: [{ id: "mbarara", name: "Mbarara" }, { id: "fortportal", name: "Fort Portal" }],
+  eastern: [{ id: "jinja", name: "Jinja" }, { id: " Mbale", name: "Mbale" }],
+  northern: [{ id: "gulu", name: "Gulu" }, { id: "laroo", name: "Lira" }],
+};
+
+export const BUSES = [
+  { id: "b1", name: "YY Express - 7:00 AM", seats: 40, price: 8.0 },
+  { id: "b2", name: "YY Comfort - 9:00 AM", seats: 32, price: 10.0 },
+  { id: "b3", name: "YY Luxury - 2:00 PM", seats: 24, price: 15.0 },
+];
